224c988ea3b748ddb8f81d754e8d418779b306fe
Aleksandar Radinkovic <acor995@gmail.com>
2022-09-22 22:35:11 +0200

    house commit 1

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer:boolean = false;
+  serverCreationStatus:string = "No server was created"
+  constructor() {
+
+   }
 
   ngOnInit(): void {
+  }
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created';
   }
 
 }
